2cc8c9f9fee5fa0c54b27bb014b30d3b359e7014
Fumitaka Tokumitsu <3142849+toku345@users.noreply.github.com>
2025-12-18 00:12:52 +0900

    refactor: add mi/collect\! to dev instrumentation for consistency
    - Add malli.instrument require to dev/user.clj
    - Call mi/collect\! before dev/start\! to ensure schema discovery
    - Aligns with test_helper.clj implementation
    
    Ref: #18 (review comment)

--- dev/user.clj
+++ dev/user.clj
@@ -1,11 +1,13 @@
 (ns user
   "Development namespace with malli instrumentation."
   (:require [malli.dev :as dev]
-            [malli.dev.pretty :as pretty]))
+            [malli.dev.pretty :as pretty]
+            [malli.instrument :as mi]))
 
 (defn start-instrumentation!
   "Start malli instrumentation with pretty error reporting."
   []
+  (mi/collect!)
   (dev/start! {:report (pretty/reporter)})
   (println "Malli instrumentation started."))
 
